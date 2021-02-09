2c213cd079a71532639c1d7befb960d5ef83d1fe
oktayudha05 <oktayudha05@gmail.com>
2021-02-09 18:04:03 +0700

    project awal

---
+++ project1.js
@@ -0,0 +1,11 @@
+var ulang = true;
+while (ulang) {
+   let pilih = prompt('pilih apa');
+   console.log(pilih);
+   
+   var ulang = confirm('lgi gk?');
+}
+
+
+let player = document.getElementById("isiP");
+let com = document.getElementById("isiC");
